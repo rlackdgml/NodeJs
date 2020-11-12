db2e2b0865fe470d63ccc01166bb6ed8d193058f
rlackdgml <dhdlqkqh2003@naver.com>
2020-11-12 16:39:43 +0900

    bucket-list

---
+++ bucket-list/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import BucketMain from "./main/BucketMain";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>&#9992;&#127759;MY BUCKET LIST&#127758;&#9972;</h2>
+      </header>
+      <BucketMain />
+    </div>
+  );
+}
+
+export default App;
---
+++ bucket-list/src/main/BucketList.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import BucketItem from "./BucketItem";
+
+class BucketList extends Component {
+  // render 밖에 어떤 함수에서 변수를 사용하는 방법
+  // 사용하고자 하는 함수내에서 parent가
+  // 보낸변수를 분해한후 사용할 준비를 해야 한다.
+  f1 = () => {
+    const { bucketList } = this.props;
+  };
+
+  render() {
+    // parent Component에서 보낸 변수를 받아서 분해한후 사용할 준비하기
+    const { bucketList, handleFlagClick, updateBucket } = this.props;
+    const bItemList = bucketList.map((bucket) => {
+      return (
+        <BucketItem
+          bucket={bucket}
+          handleFlagClick={handleFlagClick}
+          updateBucket={updateBucket}
+          handleCancel={this.props.handleCancel}
+          handleComplet={this.props.handleComplet}
+        />
+      );
+    });
+
+    return (
+      <section className="w3-container-fluid w3-margin">
+        <table className="w3-table w3-table-all">
+          <tr>
+            <th>&#127828;FLAG</th>
+            <th>&#127829;날짜</th>
+            <th>&#127830;BUCKET</th>
+            <th>&#127831;완료</th>
+            <th>&#127836;취소</th>
+          </tr>
+          {bItemList}
+        </table>
+      </section>
+    );
+  }
+}
+
+export default BucketList;
